3ce69504bbe0e35d75b8362635d21b8ee138d722
Martin Gingras <martin@mgingras.ca>
2015-04-02 16:30:27 -0400

    More versitile

--- app.js
+++ app.js
@@ -9,34 +9,40 @@ url    = require('url');
 http.createServer(function (req, res) {
   var query = url.parse(req.url, true).query;
   console.dir(query);
-  //Create TwiML response
-  var twiml = new twilio.TwimlResponse();
-  twiml.say('Yo mother fucker come on up to suite 522', {
-    voice:'woman',
-    language:'en-gb'
-  });
-  twiml.pause();
-  twiml.play({digits:'6'});
-  twiml.pause('5');
+  if(query.From === config.FRONT_DOOR_NUMBER){
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+    //Create TwiML response
+    var twiml = new twilio.TwimlResponse();
+    twiml.say('Hey, come up to suite ' + config.SUITE_NUMBER, {
+      voice:'woman',
+      language:'en-gb'
+    });
+    twiml.pause();
+    twiml.play({digits:'6'});
+    twiml.pause('5');
 
-  //Send an SMS text message
-  client.sendMessage({
-    to: config.RECIPIENT,
-    from: config.SENDER,
-    body: 'Just let someone in.'
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
 
-  }, function(err, text) { //this function is executed when a response is received from Twilio
-    if(err){
-      console.dir(err);
-    }
-    if (!err) { // "err" is an error received during the request, if any
-      console.log('Sent text to ' + config.RECIPIENT);
+    //Send an SMS text message
+    client.sendMessage({
+      to: config.RECIPIENT,
+      from: config.SENDER,
+      body: 'Just let someone in.'
+      }, function(err, text) { //this function is executed when a response is received from Twilio
+      if(err){
+        console.dir(err);
+      }
+      if (!err) { // "err" is an error received during the request, if any
+        console.log('Sent text to ' + config.RECIPIENT);
+      }
+    });
+  } else {
+    var twiml = new twilio.TwimlResponse();
+    twiml.dial(config.RECIPIENT);
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
   }
-});
-
 }).listen(port);
 
 console.log('TwiML servin\' server running on PORT: ' + port);
